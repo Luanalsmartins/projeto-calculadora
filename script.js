fbfbe949f346fb63d680902f750b29b5902c3c21
Luana Lima da Silva Martins <luanalima.silva20147@gmail.com>
2026-08-19 16:17:18 -0300

    implementado novas funções botao igual

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+
 let resultados = document.getElementById('resultados')
 const botoesNumeros = document.querySelectorAll('.numeros button')
 const botoesOperadores = document.querySelectorAll('.operadores button')
 const soOperadores = Array.from(botoesOperadores).slice(0, 5)
 const botaoC = document.querySelector('.operadores button:nth-child(7)')
 const botaoIgual = document.querySelector('.operadores button:nth-child(6)')
-
+let contaFinalizada = false
+let expressaoAnterior = ''
 
 botoesNumeros.forEach(function(botaoClicado){
+    
     botaoClicado.addEventListener('click', function(){
+        if (contaFinalizada === true) {
+            resultados.innerText = ''
+            contaFinalizada = false
+        }
         resultados.innerText += botaoClicado.textContent
     })
 })
@@ -20,8 +27,36 @@ soOperadores.forEach(function(botaoClicado){
 
 botaoC.addEventListener('click', function() {
     resultados.innerText = ''
+    contaFinalizada = false
+    expressaoAnterior = ''
 })
 
 botaoIgual.addEventListener('click', function() {
-    resultados.innerText = eval(resultados.innerText)
-})
+    let verificar = resultados.innerText[resultados.innerText.length - 1]
+    if (verificar === '+' || verificar === '-' || verificar === '*' || verificar === '/') {
+        alert('A expressão não pode terminar com um operador.')
+    } else if (contaFinalizada === false) {
+        expressaoAnterior = resultados.innerText 
+        resultados.innerText = calcular(resultados.innerText)
+        contaFinalizada = true
+    } else if (contaFinalizada === true) {
+        resultados.innerText = expressaoAnterior
+        contaFinalizada = false
+    } 
+}) 
+
+function calcular(expressao) {
+    if (expressao.includes('+')) {
+        let partes = expressao.split('+')
+        return partes.reduce((acc, numero) => acc + Number(numero), 0)
+    } else if (expressao.includes('-')) {
+        let partes = expressao.split('-')
+        return partes.reduce((acc, numero) => acc - Number(numero))
+    } else if (expressao.includes('*')) {
+        let partes = expressao.split('*')
+        return partes.reduce((acc, numero) => acc * Number(numero), 1)
+    } else if (expressao.includes('/')) {
+        let partes = expressao.split('/')
+        return partes.reduce((acc, numero) => acc / Number(numero))
+    }
+}
